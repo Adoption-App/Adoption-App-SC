3e399dbb0144dabf5741de9ea109ece91fcf5484
Cargo <ahmednafisbd15@gmail.com>
2024-06-20 09:20:30 +0800

    Nafis ends at line 85

--- Controllers/AdoptionPostsController.js
+++ Controllers/AdoptionPostsController.js
@@ -74,11 +74,12 @@ module.exports.deleteAdoptionPosts = async (req, res) =>
     } 
 
     catch (error) 
-    
+
     { 
       res.status(400).json({ 
         status: "false", 
         message: error, 
       }); 
-    } 
+    }
+     
   };
